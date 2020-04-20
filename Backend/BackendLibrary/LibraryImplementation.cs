f0be78d31081d7a6de3bfaf4fef36482369d0023
Kasper <kaha.skp@edu.mercantec.dk>
2020-04-20 16:36:28 +0200

    Basic CRUD Interface and Implementation
    Added Create, Update, Get and Delete for Machines and Fields.
    Added Commit to interface

--- Backend/BackendLibrary/LibraryImplementation.cs
+++ Backend/BackendLibrary/LibraryImplementation.cs
@@ -1,9 +1,76 @@
-﻿using System;
+﻿using BackendLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BackendLibrary {
-    class LibraryImplementation {
 
+namespace BackendLibrary {
+    class LibraryImplementation : LibraryInterface {
+        private readonly LibraryContext farmerDb;
+
+        public LibraryImplementation(LibraryContext db)
+        {
+            this.farmerDb = db;
+        }
+
+        // Database Commit
+
+        public int Commit()
+        {
+            return farmerDb.SaveChanges();
+        }
+
+        // CRUD Machines
+
+        public Machine CreateMachine(Machine newMachine)
+        {
+            farmerDb.Machines.Add(newMachine);
+            return newMachine;
+        }
+
+        public Machine GetMachine(int machineId)
+        {
+            return farmerDb.Machines.Find(machineId);
+        }
+
+        public Machine UpdateMachine(Machine updatedMachine)
+        {
+            farmerDb.Machines.Update(updatedMachine);
+            return updatedMachine;
+        }
+
+        public Machine DeleteMachine(int machineId)
+        {
+            Machine toDelete = farmerDb.Machines.Find(machineId);
+            farmerDb.Machines.Remove(toDelete);
+            return toDelete;
+        }
+
+
+        // CRUD Fields
+
+        public Field CreateField(Field newField)
+        {
+            farmerDb.Fields.Add(newField);
+            return newField;
+        }
+
+        public Field GetField(int fieldId)
+        {
+            return farmerDb.Fields.Find(fieldId);
+        }
+
+        public Field UpdateField(Field updatedField)
+        {
+            farmerDb.Fields.Update(updatedField);
+            return updatedField;
+        }
+
+        public Field DeleteField(int fieldId)
+        {
+            Field toDelete = farmerDb.Fields.Find(fieldId);
+            farmerDb.Fields.Remove(toDelete);
+            return toDelete;
+        }
     }
 }
--- Backend/BackendLibrary/LibraryInterface.cs
+++ Backend/BackendLibrary/LibraryInterface.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BackendLibrary.Models;
 
 namespace BackendLibrary {
     interface LibraryInterface {
-        //CRUD - Machines
-        //CRUD - Fields
+
+
+        // Database Commit
+
+        int Commit();
+
+        // CRUD - Machines
+
+        Machine CreateMachine(Machine newMachine);
+        Machine GetMachine(int machineId);
+        Machine UpdateMachine(Machine updatedMachine);
+        Machine DeleteMachine(int machineId);
+
+        // CRUD - Fields
+
+        Field CreateField(Field newField);
+        Field GetField(int fieldId);
+        Field UpdateField(Field updatedField);
+        Field DeleteField(int fieldId);
     }
 }
