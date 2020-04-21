7a265d7fd5d64737fbcd86e25e2311a538195c2e
Alexander Kristensen <ak@kk-partner.dk>
2020-04-21 11:58:24 +0200

    Using corrections

--- Frontend/FarmTrackingFrontend/Pages/Index.cshtml.cs
+++ Frontend/FarmTrackingFrontend/Pages/Index.cshtml.cs
@@ -1,13 +1,8 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
-using BackendLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System.Net.Http.Formatting;
+using FarmTrackingFrontend.Models;
 
 namespace FarmTrackingFrontend.Pages
 {
