9ade2aaacfc56daf86779169ec7083c9916e1ef6
Alexander Kristensen <ak@kk-partner.dk>
2020-04-22 08:40:43 +0200

    Datetime fix

---
+++ Backend/BackendLibrary/Migrations/20200422064016_DoubleToDateTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendLibrary.Migrations
+{
+    public partial class DoubleToDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RunTime",
+                table: "Machines",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "RunTime",
+                table: "Machines",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
